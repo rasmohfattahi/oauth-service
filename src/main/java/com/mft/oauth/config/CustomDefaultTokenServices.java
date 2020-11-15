1c7db2531eaa8f581d3d76f523e454d10e07bde8
rasool.fattahi <nahalfattahi2010@gmail.com>
2020-11-15 18:22:44 +0100

    implementing store parameters in AWS and upgrading spring boot parent and also cloud.

--- src/main/java/com/mft/oauth/config/CustomDefaultTokenServices.java
+++ src/main/java/com/mft/oauth/config/CustomDefaultTokenServices.java
@@ -4,7 +4,6 @@ import com.mft.general.service.Mapper;
 import com.mft.oauth.dto.UserDto;
 import com.mft.oauth.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
@@ -15,15 +14,11 @@ public class CustomDefaultTokenServices extends DefaultTokenServices {
     @Autowired
     private Mapper mapper;
 
-   @Value("${spring.mohsen}")
-    private String emad;
-
     @Override
     public OAuth2AccessToken createAccessToken(OAuth2Authentication authentication) throws AuthenticationException {
         OAuth2AccessToken oAuth2AccessToken = super.createAccessToken(authentication);
         User user = (User) authentication.getPrincipal();
         oAuth2AccessToken.getAdditionalInformation().put("user", mapper.map(user, UserDto.class));
-        oAuth2AccessToken.getAdditionalInformation().put("emad", emad);
         return oAuth2AccessToken;
     }
 
