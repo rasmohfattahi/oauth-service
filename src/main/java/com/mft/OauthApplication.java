ec26f9c05473a5fdb9ec9328d899f0f7b4b4d719
rasool.fattahi <nahalfattahi2010@gmail.com>
2021-02-07 21:46:43 +0100

    remove aws

--- src/main/java/com/mft/OauthApplication.java
+++ src/main/java/com/mft/OauthApplication.java
@@ -3,10 +3,8 @@ package com.mft;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class OauthApplication {
 
     public static void main(String[] args) {
